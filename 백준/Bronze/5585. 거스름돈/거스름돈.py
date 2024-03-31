34c3f2fd6b209e1c9b1f6a5cbcd679c06967abc2
장민석 <64572078+ms1011@users.noreply.github.com>
2024-03-31 23:30:36 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,31 +1,10 @@
-N = int(input())
-
-x = 1000 - N
+N = 1000 - int(input())
+moneys = [500, 100, 50, 10, 5, 1]
 count = 0
-while x > 0:
-    if x / 500 >= 1:
-        i = int(x / 500)
-        x -= 500
-        count += 1
-    if x / 100 >= 1:
-        i = int(x / 100)
-        x -= 100 * i
-        count += 1 * i
-    if x / 50 >= 1:
-        i = int(x / 50)
-        x -= 50 * i
-        count += 1 * i
-    if x / 10 >= 1:
-        i = int(x / 10)
-        x -= 10 * i
-        count += 1 * i
-    if x / 5 >= 1:
-        i = int(x / 5)
-        x -= 5 * i
-        count += 1 * i
-    if x / 1 >= 1:
-        i = x / 1
-        x -= 1 * i
-        count += 1 * i
+
+for money in moneys:
+    count += N // money
+    N %= money
+
 
 print(int(count))
