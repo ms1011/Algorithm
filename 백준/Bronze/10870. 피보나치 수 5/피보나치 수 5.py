8d32770b555bb4a7e8329bb67bdfc18580e26474
장민석 <64572078+ms1011@users.noreply.github.com>
2024-04-24 09:40:10 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,14 @@
+
+n = int(input())
+
+
+def fibonacci(x):
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return fibonacci(x - 1) + fibonacci(x - 2)
+
+
+print(fibonacci(n))
