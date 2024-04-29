e8af427d9ba6556d0209f37394ca73b4be736b18
장민석 <64572078+ms1011@users.noreply.github.com>
2024-04-29 21:15:48 +0900

    [Silver III] Title: 칸토어 집합, Time: 108 ms, Memory: 36652 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,17 @@
+def cut(a, n):  
+    if n == 1:
+        return
+    for i in range(a + n // 3, a + (n // 3) * 2): 
+        result[i] = ' '
+    cut(a, n // 3)
+    cut(a + n // 3 * 2, n // 3) 
+
+
+while True:
+    try:
+        N = int(input())
+        result = ['-'] * (3 ** N)
+        cut(0, 3 ** N)
+        print(''.join(result))
+    except:
+        break
