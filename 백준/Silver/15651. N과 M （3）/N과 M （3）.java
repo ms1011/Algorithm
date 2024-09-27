fdbaf592b09ddd3b1f4741ca12cb0f24c4d2f17c
ms1011 <64572078+ms1011@users.noreply.github.com>
2024-09-27 11:22:20 +0900

    [Silver III] Title: N과 M (3), Time: 372 ms, Memory: 68608 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,37 +1,83 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        selected = new int[M + 1];
-    }
-
     static int N, M;
     static int[] selected;
 
-    static void rec_func(int k) {
-        if (k == M + 1) {
-            for (int i = 1; i <= M; i++) {
-                sb.append(selected[i]).append(' ');
-            }
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[N + 1];
+    }
+
+    static void pro(int k) {
+        // 전부 골랐을 경우
+        if (k > M) {
+            for (int i = 1; i <= M; i++) sb.append(selected[i]).append(' ');
             sb.append('\n');
         } else {
-            for (int cand = 1; cand <= N; cand++) {
-                selected[k] = cand;
-                rec_func(k+1);
+            for (int i = 1; i <= N; i++) {
+                selected[k] = i;
+                
+                pro(k + 1);
                 selected[k] = 0;
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         input();
-
-        rec_func(1);
+        pro(1);
         System.out.println(sb.toString());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
