4c3d4922a39b5ca6d74493b839b0e8f8601e58e2
ms1011 <64572078+ms1011@users.noreply.github.com>
2024-10-03 16:35:20 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 404 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,27 +1,31 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    static Scanner sc = new Scanner(System.in);
 
-    static int T, N, M;
+    static int N, M;
     static int[] A, B;
 
     static void input() {
-        N = sc.nextInt();
-        M = sc.nextInt();
+        N = scan.nextInt();
+        M = scan.nextInt();
         A = new int[N + 1];
         B = new int[M + 1];
         for (int i = 1; i <= N; i++) {
-            A[i] = sc.nextInt();
+            A[i] = scan.nextInt();
         }
+
         for (int i = 1; i <= M; i++) {
-            B[i] = sc.nextInt();
+            B[i] = scan.nextInt();
         }
+
     }
 
-    static int lower_bound(int[] A, int L, int R, int X) {
+    static int lowerBound(int[] A, int L, int R, int X) {
         int res = L - 1;
+
         while (L <= R) {
             int mid = (L + R) / 2;
             if (A[mid] < X) {
@@ -31,26 +35,73 @@ public class Main {
                 R = mid - 1;
             }
         }
+
         return res;
     }
 
     static void pro() {
-        Arrays.sort(B);
+        Arrays.sort(B, 1, M + 1);
 
         int ans = 0;
+
         for (int i = 1; i <= N; i++) {
-            ans += lower_bound(B, 1, M, A[i]);
+            ans += lowerBound(B, 1, M, A[i]);
         }
         System.out.println(ans);
     }
 
-
     public static void main(String[] args) {
-        int T = sc.nextInt();
+        int T = scan.nextInt();
 
         for (int i = 0; i < T; i++) {
             input();
             pro();
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
 }
