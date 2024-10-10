f75011d7ed830378481759974961b37de562767d
ms1011 <64572078+ms1011@users.noreply.github.com>
2024-10-10 15:52:43 +0900

    [Silver IV] Title: 카드, Time: 340 ms, Memory: 34284 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -5,43 +5,37 @@ public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N;
-    static long[] a;
+    static int N, maxCnt = 1, curCnt = 1;
+    static long[] cards;
 
     static void input() {
         N = scan.nextInt();
-        a = new long[N + 1];
-        for (int i = 1; i <= N; i++) {
-            a[i] = scan.nextLong();
+        cards = new long[N];
+
+        for (int i = 0; i < N; i++) {
+            cards[i] = scan.nextLong();
         }
     }
 
     static void pro() {
-        // Sort 정렬하기
-        Arrays.sort(a, 1, N + 1);
+        Arrays.sort(cards);
 
-        // mode: 최빈값, modeCnt: 최빈값의 등장 횟수, curCnt: 현재 값(a[1])의 등장 횟수
-        long mode = a[1];
-        int modeCnt = 1, curCnt = 1;  
-        
+        long maxNum = cards[0];
 
-        // 2번 원소부터 차례대로 보면서, 같은 숫자가 이어서 나오고 있는 지, 새로운 숫자가 나왔는 지를 판단하여
-        // curCnt를 갱신해주고, 최빈값을 갱신하는 작업.
-        for (int i = 2; i <= N; i++) {
-            if (a[i - 1] == a[i]) {
+        for (int i = 1; i < N; i++) {
+            if (cards[i - 1] == cards[i]) {
                 curCnt++;
             } else {
                 curCnt = 1;
             }
 
-            if (curCnt > modeCnt) {
-                modeCnt = curCnt;
-                mode = a[i];
+            if (curCnt > maxCnt) {
+                maxCnt = curCnt;
+                maxNum = cards[i];
             }
         }
 
-        // 정답 출력하기
-        System.out.println(mode);
+        System.out.println(maxNum);
     }
 
     public static void main(String[] args) {
