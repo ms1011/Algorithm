faa779f00226be1569f597cdd12a6b43bac833ef
ms1011 <64572078+ms1011@users.noreply.github.com>
2024-10-29 11:15:57 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 132 ms, Memory: 16252 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static String X, Y;
+
+    static void input() {
+        String[] input = scan.nextLine().split(" ");
+        X = input[0];
+        Y = input[1];
+    }
+
+    static int reverse(String str) {
+        String reversedString = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedString += str.charAt(i);
+        }
+
+        return Integer.valueOf(reversedString);
+    }
+
+    static void pro() {
+        int xy = reverse(X) + reverse(Y);
+        System.out.println(reverse(String.valueOf(xy)));
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
